41e72abac510f74ade7afaa3b6fdcf34f6ff9eb3
Lawrenzo Lue <lawrenzo.lue@macaulay.cuny.edu>
2025-10-17 09:16:33 -0400

    pre merge

--- popup/popup.js
+++ popup/popup.js
@@ -58,22 +58,22 @@ function testInput(e) {
 //   console.log(data);
 //   console.log(data.food);
 // });
-testItem = [
-  {
-    name: 'egg',
-    calories: 'Only available for premium subscribers.',
-    serving_size_g: 'Only available for premium subscribers.',
-    fat_total_g: 9.7,
-    fat_saturated_g: 3.1,
-    protein_g: 'Only available for premium subscribers.',
-    sodium_mg: 139,
-    potassium_mg: 199,
-    cholesterol_mg: 371,
-    carbohydrates_total_g: 0.7,
-    fiber_g: 0,
-    sugar_g: 0.4,
-  },
-];
+// testItem = [
+//   {
+//     name: 'egg',
+//     calories: 'Only available for premium subscribers.',
+//     serving_size_g: 'Only available for premium subscribers.',
+//     fat_total_g: 9.7,
+//     fat_saturated_g: 3.1,
+//     protein_g: 'Only available for premium subscribers.',
+//     sodium_mg: 139,
+//     potassium_mg: 199,
+//     cholesterol_mg: 371,
+//     carbohydrates_total_g: 0.7,
+//     fiber_g: 0,
+//     sugar_g: 0.4,
+//   },
+// ];
 
 // async function getFood() {
 //   console.log(input);
